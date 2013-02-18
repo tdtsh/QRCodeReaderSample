8333e88dcd778b1308af54ed5032ccc33ef650a1
tdtsh <systemsfailing@gmail.com>
2013-02-18 18:27:36 +0900

    色々

--- QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderViewController.h
+++ QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderViewController.h
@@ -6,22 +6,6 @@
 //  Copyright (c) 2012年 tadatoshi_hanazaki. All rights reserved.
 //
 
-///Users/tadatoshi_hanazaki/workspace/QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderViewController.h:10:9: 'ZXingWidgetController.h' file not found
-
-///Users/tadatoshi_hanazaki/workspace/QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderViewController.h:10:9: 'ZXingWidgetController.h' file not found
-
-///Users/tadatoshi_hanazaki/workspace/QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderViewController.h:10:9: 'ZXingWidgetController.h' file not found
-
-/*
-/Users/tadatoshi_hanazaki/workspace/QRCodeReaderSample/ZXing/iphone/ZXingWidget/Classes
-/Users/tadatoshi_hanazaki/workspace/QRCodeReaderSample/ZXing/cpp/core/src
-../../iphone/ZXingWidget/Classes
-../../cpp/core/src
-
-*/
-
-//(null): "std::string::c_str() const", referenced from:
-
 #import <UIKit/UIKit.h>
 #import "ZXingWidgetController.h"
 
--- QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderViewController.mm
+++ QRCodeReaderSample/QRCodeReaderSample/QRCodeReaderViewController.mm
@@ -20,62 +20,67 @@
  
 - (void)scanPressed:(id)sender
 {
-    ZXingWidgetController *widController = [[ZXingWidgetController alloc] initWithDelegate:self showCancel:YES OneDMode:NO];
-    QRCodeReader* qrcodeReader = [[QRCodeReader alloc] init];
-    NSSet *readers = [[NSSet alloc ] initWithObjects:qrcodeReader,nil];
-    //[qrcodeReader release];
-    widController.readers = readers;
-    //[readers release];
+	ZXingWidgetController *widController = [[ZXingWidgetController alloc] initWithDelegate:self showCancel:YES OneDMode:NO];
+	QRCodeReader* qrcodeReader = [[QRCodeReader alloc] init];
+	NSSet *readers = [[NSSet alloc ] initWithObjects:qrcodeReader,nil];
+	widController.readers = readers;
  
-    [self presentModalViewController:widController animated:YES];
-    //[widController release];
+	[self presentModalViewController:widController animated:YES];
 }
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-    }
-    return self;
+	self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+	if (self) 
+	{
+	}
+	return self;
 }
+
 - (void)dealloc {
-    //[resultView release];
+	//[resultView release];
 }
  
 #pragma mark - View lifecycle
 - (void)loadView
 {
-    [super loadView];
-     
-    UIButton *btnStamp = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [btnStamp setFrame:CGRectMake(40, 220, 240, 50)];
-    [btnStamp setTitle:@"QRコードを読み取る" forState:UIControlStateNormal];
-    [btnStamp setTag:1];
-    [btnStamp addTarget:self action:@selector(scanPressed:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btnStamp];
-     
-    resultView = [[UITextView alloc] initWithFrame:CGRectMake(40 , 280, 240, 50)];
-    [self.view addSubview:resultView];
+	[super loadView];
+	 
+	UIButton *btnStamp = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+	[btnStamp setFrame:CGRectMake(40, 220, 240, 50)];
+	[btnStamp setTitle:@"QRコードを読み取る" forState:UIControlStateNormal];
+	[btnStamp setTag:1];
+	[btnStamp addTarget:self action:@selector(scanPressed:) forControlEvents:UIControlEventTouchUpInside];
+	[self.view addSubview:btnStamp];
+	 
+	resultView = [[UITextView alloc] initWithFrame:CGRectMake(40 , 280, 240, 50)];
+	[self.view addSubview:resultView];
 }
+
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
+	[super viewDidUnload];
 }
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
  
 #pragma mark -
 #pragma mark ZXingDelegateMethods
 - (void)zxingController:(ZXingWidgetController*)controller didScanResult:(NSString *)result {
-    self.resultStr = result;
-    if (self.isViewLoaded) {
-        [resultView setText:resultStr];
-        [resultView setNeedsDisplay];
-    }
-    [self dismissModalViewControllerAnimated:NO];
+	self.resultStr = result;
+	if (self.isViewLoaded) 
+	{
+		[resultView setText:resultStr];
+		[resultView setNeedsDisplay];
+	}
+	[self dismissModalViewControllerAnimated:NO];
 }
+
 - (void)zxingControllerDidCancel:(ZXingWidgetController*)controller {
-    [self dismissModalViewControllerAnimated:YES];
+	[self dismissModalViewControllerAnimated:YES];
 }
+
 @end
